0b3c1597035aa54592674a7d5e6aaae24ee90a6f
Arkadiusz.Palka <0arek98@gmail.com>
2018-03-22 14:14:20 +0100

    first commit

---
+++ app/src/main/java/arkadiuszpalka/quizomania/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package arkadiuszpalka.quizomania.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import arkadiuszpalka.quizomania.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
